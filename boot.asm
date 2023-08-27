d87093ff24da65da8cc1a59768662a147871a8d6
jingyusu <gar123su@gmail.com>
2023-08-27 05:04:40 -0400

    add code to enable protected mode

--- boot.asm
+++ boot.asm
@@ -1,5 +1,9 @@
-ORG 0
+ORG 0x7c00
 BITS 16
+
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
+
 _start: ; required by BPB of some bios
     jmp short start
     nop
@@ -7,57 +11,66 @@ _start: ; required by BPB of some bios
 times 33 db 0 ; create a fake BPB in case of BIOS rewrite
 
 start:
-    jmp 0x7c0:execute
+    jmp 0:execute
 
 execute:
     cli ; Clear Interrupts
-    mov ax, 0x7c0
+    mov ax, 0x00
     mov ds, ax
     mov es, ax
-    mov ax, 0x00
     mov ss, ax
     mov sp, 0x7c00
     sti ; Enable Interrupts
 
-    mov ah, 2 ; Read sector cmd (CHS)
-    mov al, 1 ; Read one sector
-    mov ch, 0 ; Cylinder low eight bits
-    mov cl, 2 ; Read sector 2 (1-indexed)
-    mov dh, 0 ; Head number
-    mov bx, buffer ; Set dest addr
-    int 0x13
-    jc error
+.load_protected: 
+    cli            ; disable interrupts
+    lgdt [gdt_descriptor]    ; load GDT register with start address of Global Descriptor Table
+    mov eax, cr0 
+    or al, 1       ; set PE (Protection Enable) bit in CR0 (Control Register 0)
+    mov cr0, eax
+    jmp CODE_SEG:load32
 
-    mov si, buffer
-    call print
-    
+; GDT
+gdt_start:
+gdt_null: 
+    dd 0x0
+    dd 0x0
+
+; offset 0x8
+gdt_code:     ; CS SHOULD POINT to this
+    dw 0xffff ; Segment limit first 0-15 bits (limit is maximum addressible unit, if choose paging and 4kb granularity, set limit=0xfffff(yes 20 bits), address spans entire 4GB addr space)
+    dw 0      ; Base first 0-15 bits (Base addr of the segment)
+    db 0      ; Base 16-23 bits
+    db 0x9a   ; Access byte (Access bit mask)
+    db 11001111b ; High and low 4 bit flags
+    db 0      ; Base 24-31 bits
+
+; Offset 0x10
+gdt_data:      ; DS, SS, ES, FS, GS SHOULD POINT TO THIS
+    dw 0xffff ; Segment limit first 0-15 bits (limit is maximum addressible unit, if choose paging and 4kb granularity, set limit=0xfffff(yes 20 bits), address spans entire 4GB addr space)
+    dw 0      ; Base first 0-15 bits (Base addr of the segment)
+    db 0      ; Base 16-23 bits
+    db 0x92   ; Access byte (Access bit mask)
+    db 11001111b ; High and low 4 bit flags
+    db 0      ; Base 24-31 bits
+
+gdt_end:
+
+gdt_descriptor:
+    dw gdt_end - gdt_start - 1
+    dd gdt_start
+
+[BITS 32]
+load32:
+    mov ax, DATA_SEG
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    mov ebp, 0x00200000
+    mov esp, ebp
     jmp $
-
-error:
-    mov si, error_message
-    call print
-    jmp $
-
-print:
-    mov bx, 0
-.loop:    
-    lodsb
-    cmp al, 0
-    je .done
-    call print_char
-    jmp .loop
-.done:    
-    ret
-
-print_char:
-    mov ah, 0eh
-    int 0x10
-    ret
-
-
-error_message: db 'Failed to load sector', 0
 
 times 510-($ - $$) db 0
 dw 0xAA55
-
-buffer: 
